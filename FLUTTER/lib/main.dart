3bac4f142edc1ce2722d55ce439ab04a8c235d5c
Yunan Maulana <maulanayunan2@gmail.com>
2025-08-26 11:44:24 +0900

    Biodata

--- FLUTTER/lib/main.dart
+++ FLUTTER/lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Kamu menekan tombol sebanyak:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
